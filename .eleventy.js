acc84317769c80b390def7b83e88eaede48a0dda
Ruben Klink <10252030+j0holo@users.noreply.github.com>
2026-04-21 21:42:28 +0200

    Update Eleventy and add sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,28 @@
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight'),
-  markdownIt = require('markdown-it'),
-  pluginRss = require('@11ty/eleventy-plugin-rss');
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight"),
+  markdownIt = require("markdown-it"),
+  pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPassthroughCopy('favicon.ico');
-  eleventyConfig.addPassthroughCopy('assets/images');
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("assets/images");
   const options = {
     html: true,
     breaks: true,
-    linkify: false
+    linkify: false,
   };
-  eleventyConfig.setLibrary('md', markdownIt(options));
+  eleventyConfig.setLibrary("md", markdownIt(options));
+
+  eleventyConfig.addFilter("isoDate", (date) => {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, "0");
+    const day = String(d.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  });
 
   return {
     // Use liquid in html templates
-    htmlTemplateEngine: 'liquid'
+    htmlTemplateEngine: "liquid",
   };
 };
